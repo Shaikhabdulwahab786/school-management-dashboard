18129a95f4350b36f0904835e49e3265f9a48cc5
shaikh abdul wahhab <adshaikh837@gmail.com>
2026-01-20 18:34:29 +0530

    fix signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,15 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
 const Homepage = () => {
+   const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  redirect("/redirect");
+
   return (
     <div className=''>Homepage</div>
   )
