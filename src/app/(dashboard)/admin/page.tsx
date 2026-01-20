4b6356dc0d9522ec5015c4ca625443c6f24df81e
shaikh abdul wahhab <adshaikh837@gmail.com>
2026-01-20 19:48:45 +0530

    change admin

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,16 +1,15 @@
+// src/app/(dashboard)/admin/page.tsx
+import dynamic from "next/dynamic";
 
-import Announcements from "@/components/Announcements";
-import AttendanceChartContainer from "@/components/AttendanceChartContainer";
-import CountChartContainer from "@/components/CountChartContainer";
-import EventCalendarContainer from "@/components/EventCalendarContainer";
-import FinanceChart from "@/components/FinanceChart";
-import UserCard from "@/components/UserCard";
+// Dynamically import all client components with SSR disabled
+const UserCard = dynamic(() => import("@/components/UserCard"), { ssr: false });
+const CountChartContainer = dynamic(() => import("@/components/CountChartContainer"), { ssr: false });
+const AttendanceChartContainer = dynamic(() => import("@/components/AttendanceChartContainer"), { ssr: false });
+const FinanceChart = dynamic(() => import("@/components/FinanceChart"), { ssr: false });
+const EventCalendarContainer = dynamic(() => import("@/components/EventCalendarContainer"), { ssr: false });
+const Announcements = dynamic(() => import("@/components/Announcements"), { ssr: false });
 
-const AdminPage = ({
-  searchParams,
-}: {
-  searchParams: { [keys: string]: string | undefined };
-}) => {
+const AdminPage = ({ searchParams }: { searchParams: { [key: string]: string | undefined } }) => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
@@ -22,25 +21,22 @@ const AdminPage = ({
           <UserCard type="student" />
           <UserCard type="parent" />
         </div>
-        {/* MIDDLE CHARTS */}
+        {/* CHARTS */}
         <div className="flex gap-4 flex-col lg:flex-row">
-          {/* COUNT CHART */}
           <div className="w-full lg:w-1/3 h-[450px]">
             <CountChartContainer />
           </div>
-          {/* ATTENDANCE CHART */}
           <div className="w-full lg:w-2/3 h-[450px]">
             <AttendanceChartContainer />
           </div>
         </div>
-        {/* BOTTOM CHART */}
         <div className="w-full h-[500px]">
           <FinanceChart />
         </div>
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        <EventCalendarContainer searchParams={searchParams}/>
+        <EventCalendarContainer searchParams={searchParams} />
         <Announcements />
       </div>
     </div>
